5c1f268c4077f4e38a2b7dfbe388f4ba56f40a53
Ondrej Platek <ondrej.platek@seznam.cz>
2014-03-05 18:52:18 +0100

    start real experiment among PTICS setup

--- collect_data.sh
+++ collect_data.sh
@@ -11,25 +11,25 @@ echo "    2) How length of wav influence decoding"
 
 pykaldi_dir=..
 
-# for b in 13 14 15 16 16.5 ; do
-#     for lb in 5.5 6 6.5 7 10 ; do 
-#         for ma in 2000 6000 12000 ; do
-
+# Used args Alex PTICS
+# --max-mem=10000000000 --lat-lm-scale=10 --beam=12.0 --lattice-beam=6.0 --max-active=5000', 
+# pg CLASS LM weight 0.8 uniform sub sampling td 0.90 tri2b_bmmi
 pushd $pykaldi_dir
 
 wav_scp=pykaldi-eval/test_16_wer/input.scp
         # for ma in 2000 6000 12000 ; do
-for lb in  2 3 4.5 6 8 10 ; do 
-    for b in 13 14 15 16 16.5 ; do
-        echo; echo "Running for $wav_scp:"
-        echo "lb $lb ; b $b ; ma $ma"; echo
+for ma in 5000 2000 8000 ; do
+    for lb in  2 3 4.5 6 7 8 ; do 
+        for b in 8 11 12 13 16 ; do
+            echo; echo "Running for $wav_scp:"
+            echo "lb $lb ; b $b ; ma $ma"; echo
 
-        # all 50 recodings
-        ./run_pykaldi-latgen-faster-decoder.sh --wav_scp $wav_scp --latbeam $lb --beam $b || exit 1
-        # ./run_pykaldi-latgen-faster-decoder.sh --wav_scp $wav_scp --latbeam $lb --beam $b --max_active $ma || exit 1
+            ./run_pykaldi-latgen-faster-decoder.sh \
+                --wav_scp $wav_scp --latbeam $lb --beam $b --max_active $ma \
+                || exit 1
 
+        done
     done
 done
-# done
 
 popd
