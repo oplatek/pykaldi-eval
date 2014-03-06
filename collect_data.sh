e805401f005b7d8bc4baa5da90d73a7c7c0b25eb
Ondrej Platek <ondrej.platek@seznam.cz>
2014-03-06 20:50:23 +0100

    Fixed major bug... running all the same setup

--- collect_data.sh
+++ collect_data.sh
@@ -47,7 +47,7 @@ for ma in 5000 2000 8000 ; do
             kernprof.py -o pykaldi-eval/kern-log/kern_prof_${logname}.log -l -v \
               pykaldi-latgen-faster-decoder.py $wav_scp $batch_size $pykaldi_latgen_tra $WST \
                 --verbose=0  --max-mem=1000000000000 --lat-lm-scale=15 --config=$MFCC \
-                --beam=$beam --lattice-beam=$latbeam --max-active=$max_active \
+                --beam=$b --lattice-beam=$lb --max-active=$ma \
                 $AM $HCLG `cat $SILENCE` $MAT  >> $log &
         done
 
